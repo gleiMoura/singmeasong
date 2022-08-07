8b8381e5d52825a31628d6443b176d102ebb4d4c
Gleison Moura <gle.bar.mou@gmail.com>
2022-08-07 20:30:56 -0300

    create a integration test to route /recommendations/random/:amount

--- sing-me-a-song-main/back-end/tests/integration/recommendation.test.ts
+++ sing-me-a-song-main/back-end/tests/integration/recommendation.test.ts
@@ -167,7 +167,7 @@ describe("Recomendations test - integration", () => {
 
         let percent = (nominator/denominator);
         console.log("percent have to be 0.7 ---> ", percent)
-        if(percent > 0.65 && percent < 0.75) {
+        if(percent > 0.6 && percent < 0.8) {
             percent = 0.7
         }
 
@@ -221,7 +221,7 @@ describe("Recomendations test - integration", () => {
         expect(percent).toBe(0.3);
     });
 
-    it("should get catch any recommendation if only exist upvotes with 1- score or more", async () => {
+    it("should get catch any recommendation if only exist upvotes with 10 score or more", async () => {
         const maxLenth = 20;
         const minLength = 10;
         const arrLength = recommendationFactory.createRandomNumber(maxLenth, minLength);
@@ -319,6 +319,36 @@ describe("Recomendations test - integration", () => {
         const response = await supertest(app).get("/recommendations/random");
 
         expect(response.statusCode).toBe(404);
+    });
+
+    it("Should get a number of top recommendations", async () => {
+        const maxLenth = 20;
+        const minLength = 10;
+        const arrLength = recommendationFactory.createRandomNumber(maxLenth, minLength);
+
+        const maxScore = 50;
+        const minScore = -5;
+        const data = [];
+
+        for(let i = 0; i < arrLength; i++){
+            const scoreNumber = recommendationFactory.createRandomNumber(maxScore, minScore);
+
+            data.push({
+                name: faker.name.findName(),
+                youtubeLink: 'https://youtu.be/1bFz-SVX98g',
+                score: scoreNumber
+              })
+        }
+
+        await prisma.recommendation.createMany({ data });
+
+        const amount = 10;
+        const response = await supertest(app).get(`/recommendations/top/${amount}`);
+        const findRecommendations = await prisma.recommendation.findMany({ take: amount, orderBy: { score: 'desc' } });
+    
+        expect(response.body.length).toBe(amount);
+        expect(response.body).toStrictEqual(findRecommendations);
+        expect(response.statusCode).toBe(200);
     });
 });
 
